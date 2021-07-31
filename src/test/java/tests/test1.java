9827e4b96eeaad4ce13d08cbe54dd0570630fd01
balu <balu@DESKTOP-FL9H9Q7>
2021-07-31 17:54:28 +0530

    my first test

---
+++ src/test/java/tests/test1.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class test1 
+{
+	@Test
+	public void method() 
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("sum ot two numbers"+c);
+		
+	}
+
+}
